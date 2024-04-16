1f5f9441d2d2fdbc5dcaf8d78616270ea81f22e1
Diogo Franco <dmfspfc12@gmail.com>
2024-04-16 16:01:36 -0300

    test

--- gks_components/__init__.py
+++
@@ -1,4 +0,0 @@
-IS_RELEASE = True
-
-from .GksTabBar import tab_bar
-from .GksTabBar import TabBarItemData
---
+++ gkscomponents/__init__.py
@@ -0,0 +1,4 @@
+IS_RELEASE = True
+
+from .TabBar import tab_bar
+from .TabBar import TabBarItemData
